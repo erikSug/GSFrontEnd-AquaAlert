0735f7dc2f0d5740c51e301f475eedf81b1a3f7d
Erik Kaiyu Suguiyama <eriksug4@gmail.com>
2025-06-04 21:29:25 -0300

    feat(cidade/animação): Adicionando animação para o alerta

--- script.js
+++ script.js
@@ -2,30 +2,37 @@ const btn = document.getElementById('menu-btn');
 const menu = document.getElementById('menu');
 let cidade = document.getElementById('demo-cidade');
 let slider = document.getElementById('water-level');
+let alertAnimation = document.getElementById('ping');
 let waterLevel = 0;
-
-
-
-
-
-function checkLevel(){
-    if (waterLevel >= 70){
-        cidade.style.backgroundColor = '#FF1C1C'
-    }
-    else if (waterLevel >= 40){
-        cidade.style.backgroundColor = '#FFEC1C'
-    }
-    else{
-        cidade.style.backgroundColor = '#1CFF46'
-    }
-}
 
 
 btn.addEventListener('click', () => {
     menu.classList.toggle('hidden');
 });
 
+
+function checkLevel(){
+    if (waterLevel >= 70){
+        cidade.style.backgroundColor = '#FF1C1C'
+        alertAnimation.style.backgroundColor = '#FF1C1C'
+        alertAnimation.style.display = 'block'
+    }
+    else if (waterLevel >= 40){
+        cidade.style.backgroundColor = '#FFEC1C'
+        alertAnimation.style.backgroundColor = '#FFEC1C'
+        alertAnimation.style.display = 'block'
+    }
+    else{
+        cidade.style.backgroundColor = '#1CFF46'
+        alertAnimation.style.backgroundColor = '#1CFF46'
+        alertAnimation.style.display = 'none'
+    }
+}
+
+
+
+
 slider.oninput = function (){
     waterLevel = this.value;
 }
-setInterval( checkLevel, 2000);
+setInterval(checkLevel, 1000);
